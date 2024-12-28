f685ac05c6c9413027b492493c8f3e4808aa6b1a
defk0n1 <zrigua.ghassen@gmail.com>
2024-12-28 21:58:41 +0100

    wed backend

--- amazon.py
+++ amazon.py
@@ -1,14 +1,11 @@
 from selectorlib import Extractor
-import requests 
-import json 
-from time import sleep
-
+import json
+import requests
 
 # Create an Extractor by reading from the YAML file
 e = Extractor.from_yaml_file('selectors.yml')
 
-def scrape(url):  
-
+def scrape(url):
     headers = {
         'dnt': '1',
         'upgrade-insecure-requests': '1',
@@ -22,25 +19,54 @@ def scrape(url):
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
 
-    # Download the page using requests
-    print("Downloading %s"%url)
+    print(f"Downloading {url}")
     r = requests.get(url, headers=headers)
-    # Simple check to check if page was blocked (Usually 503)
     if r.status_code > 500:
         if "To discuss automated access to Amazon data please contact" in r.text:
-            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+            print(f"Page {url} was blocked by Amazon. Please try using better proxies")
         else:
-            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+            print(f"Page {url} must have been blocked by Amazon as the status code was {r.status_code}")
         return None
-    # Pass the HTML of the page and create 
-    return e.extract(r.text)
 
-# product_data = []
-with open("urls.txt",'r') as urllist, open('output.jsonl','w') as outfile:
-    for url in urllist.read().splitlines():
-        data = scrape(url) 
-        if data:
-            json.dump(data,outfile)
-            outfile.write("\n")
-            # sleep(5)
-    
+    # Extract data using the selector
+    extracted_data = e.extract(r.text)
+
+    if not extracted_data:
+        return None
+
+    # Extracting images data and parsing it if present
+    if 'images' in extracted_data and extracted_data['images']:
+        dynamic_image_data = json.loads(extracted_data['images'])
+
+        # Construct image links based on size
+        image_links = {}
+        for link, size in dynamic_image_data.items():
+            size_key = f"{size[0]}x{size[1]}"
+            image_links[size_key] = link
+    else:
+        image_links = {}
+
+    # Extract other product details
+    title = extracted_data.get('name', None)
+    price = extracted_data.get('price', None)
+    short_description = extracted_data.get('short_description', None)
+    rating = extracted_data.get('rating', None)
+    number_of_reviews = extracted_data.get('number_of_reviews', None)
+    variants = extracted_data.get('variants', [])
+    product_description = extracted_data.get('product_description', None)
+    sales_rank = extracted_data.get('sales_rank', None)
+    link_to_all_reviews = extracted_data.get('link_to_all_reviews', None)
+
+    # Return all the extracted product details
+    return {
+        "title": title,
+        "price": price,
+        "short_description": short_description,
+        "images": image_links,
+        "rating": rating,
+        "number_of_reviews": number_of_reviews,
+        "variants": variants,
+        "product_description": product_description,
+        "sales_rank": sales_rank,
+        "link_to_all_reviews": link_to_all_reviews
+    }
